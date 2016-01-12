392d116cef61904aa3a678d84925e680d615e66b
Rüdiger Klaehn <rklaehn@gmail.com>
2016-01-12 20:21:54 +0100

    Commented out some code
    Still breaks :-/

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,10 +5,10 @@ import akka.http.scaladsl.model.MediaTypes
 object Main {
 
   val x = Map(
-    "csv" -> MediaTypes.`text/csv`,
-    "tsv" -> MediaTypes.`text/tab-separated-values`,
+//    "csv" -> MediaTypes.`text/csv`,
+//    "tsv" -> MediaTypes.`text/tab-separated-values`,
     "xml" -> MediaTypes.`application/xml`,
-    "json" -> MediaTypes.`application/json`,
-    "html" -> MediaTypes.`text/html`
+    "json" -> MediaTypes.`application/json`
+//    "html" -> MediaTypes.`text/html`
   )
 }
